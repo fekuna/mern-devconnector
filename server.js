231dca5d1c8e0846a1889fb7913a152e6967728c
fekuna <hichirro@gmail.com>
2019-04-03 11:51:33 +0700

    prepared for deploy

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const path = require('path');
 
 // Fetch Api
 const authRoutes = require('./routes/api/auth');
@@ -11,29 +12,31 @@ const profileRoutes = require('./routes/api/profile');
 const app = express();
 
 // bodyParser middleware
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // DB config
 const { mongoURI } = require('./config/keys');
 // Connect to mongoDB
 mongoose
-	.connect(mongoURI, {useNewUrlParser: true})
-	.then(() => {
-		console.log('MongoDB Connected');
-	})
-	.catch((err) => {
-		console.log(err);
-	});
+  .connect(mongoURI, { useNewUrlParser: true })
+  .then(() => {
+    console.log('MongoDB Connected');
+  })
+  .catch(err => {
+    console.log(err);
+  });
 
 // CORS
 app.use((req, res, next) => {
-	res.setHeader('Access-Control-Allow-Origin', '*');
-	res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
-	res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-	next();
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
 });
-
 
 // Passport middleware
 app.use(passport.initialize());
@@ -45,6 +48,16 @@ require('./config/passport')(passport);
 app.use('/api/users', authRoutes);
 app.use('/api/profile', profileRoutes);
 app.use('/api/posts', postsRoutes);
+
+// Server static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
